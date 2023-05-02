00b1d4fa2279dc6acc1387a9a9745a96238d8afa
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-05-02 14:23:22 +0300

    feat: footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,26 @@
 import styles from './Footer.module.scss';
+import reactLogo from '../../assets/react.svg';
 
 export default function Footer() {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <footer className={styles.row}>
+      <div className={styles.link}>
+        <a href="https://github.com/vyacheslavmusiienko" target="_blank" rel="noopener noreferrer">
+          Viacheslav Musiienko
+        </a>
+        <a href="https://github.com/sbavia" target="_blank" rel="noopener noreferrer">
+          Ivan Nikanau
+        </a>
+        <a href="https://github.com/luferov1" target="_blank" rel="noopener noreferrer">
+          Dzmitry Luferau
+        </a>
+      </div>
+      <div className="data">2023</div>
+      <div className="logo">
+        <a href="https://rs.school/react/" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </footer>
+  );
 }
