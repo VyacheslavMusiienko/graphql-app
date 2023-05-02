247c526db2eb0cf8904f2f85821b1e87f2658a3f
Luferov1 <ldima12022002@gmail.com>
2023-05-02 17:55:53 +0300

    refactor: change strings to enum values

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,22 @@
 import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
+import { Paths } from '../../utils/enums';
 export default function Header() {
   return (
     <header>
       <nav>
         <ul className={styles.row}>
           <li className={styles.row_item}>
-            <NavLink to="/">Main</NavLink>
+            <NavLink to={Paths.Main}>Main</NavLink>
           </li>
           <li className={styles.row_item}>
-            <NavLink to="/form">Form</NavLink>
+            <NavLink to={Paths.Form}>Form</NavLink>
           </li>
           <li className={styles.row_item}>
-            <NavLink to="/graphql">GraphQl</NavLink>
+            <NavLink to={Paths.GraphQL}>GraphQl</NavLink>
           </li>
         </ul>
       </nav>
     </header>
   );
-}
+};
