8643eada3aa28d2ae39c913473912bf2ee4a16e1
Ivan Nikanau <somemore17@gmail.com>
2023-05-10 21:59:17 +0300

    fix: make redirect from auth pages if already logged in

--- src/pages/AuthPages/LoginPage.tsx
+++ src/pages/AuthPages/LoginPage.tsx
@@ -1,8 +1,27 @@
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+
 import LoginForm from './LoginForm';
+
+import { useAppDispatch, authSlice } from '../../store';
+import { auth } from '../../firebase';
 
 import styles from './AuthPages.module.scss';
 
 const LoginPage = () => {
+  const { setUser } = authSlice.actions;
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      dispatch(setUser(user));
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <LoginForm />
