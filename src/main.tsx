21ad2d8daeaa57d6e2e356621341fb56d3345a7f
Luferov1 <ldima12022002@gmail.com>
2023-05-11 19:10:43 +0300

    refactor: some changes after merging

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 import router from './Router';
 import './index.scss';
-import { setupStore } from './store/store';
+import { setupStore } from './store';
 import './utils/i18';
 
 const store = setupStore();
