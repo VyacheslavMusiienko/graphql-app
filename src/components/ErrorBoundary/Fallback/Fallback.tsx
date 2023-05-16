23ae4ee5cf3f39c72385bb0bac0b8372d0e2d2e0
Luferov1 <ldima12022002@gmail.com>
2023-05-16 18:59:01 +0300

    feat: add translation

--- src/components/ErrorBoundary/Fallback/Fallback.tsx
+++ src/components/ErrorBoundary/Fallback/Fallback.tsx
@@ -1,12 +1,16 @@
-import styles from './Fallback.module.scss';
+import { useTranslation } from 'react-i18next';
 import ExclamationPoint from '../../ExclamationPoint/ExclamationPoint';
+import styles from './Fallback.module.scss';
 
-const Fallback = () => (
-  <div className={styles.container}>
-    <ExclamationPoint />
-    <h2 className={styles.title}>Something went wrong...</h2>
-    <p className={styles.text}>Please try again later</p>
-  </div>
-);
+const Fallback = () => {
+  const { t } = useTranslation();
+  return (
+    <div className={styles.container}>
+      <ExclamationPoint />
+      <h2 className={styles.title}>{t('errorBoundary', { context: 'title' })}</h2>
+      <p className={styles.text}>{t('errorBoundary', { context: 'text' })}</p>
+    </div>
+  );
+};
 
 export default Fallback;
