42f9b1e6335f6f059fcbc55ef86dcf2dfe868aa4
Ivan Nikanau <somemore17@gmail.com>
2023-05-18 15:02:04 +0300

    feat: add team section

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useTranslation } from 'react-i18next';
 import { LoginButton, SignUpButton, GoToButton } from '../../components/button';
 import AuthStatus from '../../components/authStatus';
 import Footer from '../../components/Footer';
@@ -5,10 +7,15 @@ import Footer from '../../components/Footer';
 import useAuth from '../../hooks/useAuth';
 
 import { styles } from '../../layout';
-// import st from './welcome.module.scss';
+import st from './welcome.module.scss';
+
+import Ivan from '../../assets/ivan.jpg';
+import Dzmitry from '../../assets/dzmitry.jpg';
+import Vyacheslav from '../../assets/vyacheslav.jpg';
 
 const WelcomePage = () => {
   const { user } = useAuth();
+  const { t } = useTranslation();
 
   return (
     <div className={styles.wrapper}>
@@ -25,6 +32,29 @@ const WelcomePage = () => {
             <SignUpButton />
           </>
         )}
+        <section className={st.teamSection}>
+          <div className={st.teamMember}>
+            <img src={Vyacheslav} alt="Team Member 1" className={st.memberPhoto} />
+            <div className={st.memberInfo}>
+              <h3 className={st.memberName}>{t('Vyacheslav')}</h3>
+              <p className={st.memberDescription}>ToDo description 1</p>
+            </div>
+          </div>
+          <div className={st.teamMember}>
+            <img src={Ivan} alt="Team Member 2" className={st.memberPhoto} />
+            <div className={st.memberInfo}>
+              <h3 className={st.memberName}>{t('Ivan')}</h3>
+              <p className={st.memberDescription}>ToDo description 2</p>
+            </div>
+          </div>
+          <div className={st.teamMember}>
+            <img src={Dzmitry} alt="Team Member 3" className={st.memberPhoto} />
+            <div className={st.memberInfo}>
+              <h3 className={st.memberName}>{t('Dzmitry')}</h3>
+              <p className={st.memberDescription}>ToDo description 3</p>
+            </div>
+          </div>
+        </section>
       </div>
       <Footer />
     </div>
