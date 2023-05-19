e469ab1e76df5dec212e441abe20d8827245a9c7
VyacheslavMusiienko <chuvachok98@gmail.com>
2023-05-19 11:40:21 +0300

    feat: add character schema

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -11,6 +11,7 @@ import Loader from '../loader';
 import { useAppSelector } from '../../store';
 
 import styles from './CodeEditor.module.scss';
+import CharacterSchema from './CharacterSchema/CharacterSchema';
 
 const CharacterSchema = lazy(() => import('./CharacterSchema/CharacterSchema'));
 
