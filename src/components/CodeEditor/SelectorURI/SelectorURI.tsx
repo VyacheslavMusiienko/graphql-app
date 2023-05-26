674562f4e311402aa56e909cec2c08e0d95c518d
Ivan Nikanau <somemore17@gmail.com>
2023-05-26 11:38:57 +0300

    fix: translation of select schema

--- src/components/CodeEditor/SelectorURI/SelectorURI.tsx
+++ src/components/CodeEditor/SelectorURI/SelectorURI.tsx
@@ -1,11 +1,15 @@
+import { useTranslation } from 'react-i18next';
+
 import { useAppDispatch, useAppSelector } from '../../../store';
 import { EditorSlice } from '../../../store/reducers/EditorSlice';
+
 import styles from './SelectorURI.module.scss';
 
 const SelectorURI = () => {
   const { schemaURI } = useAppSelector((state) => state.EditorReducer);
   const { setSchema } = EditorSlice.actions;
   const dispatch = useAppDispatch();
+  const { t } = useTranslation();
 
   function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
     dispatch(setSchema(event.target.value));
@@ -13,7 +17,7 @@ const SelectorURI = () => {
 
   return (
     <div className={styles.main}>
-      <div>Select schema</div>
+      <div>{t('selectSchema')}</div>
       <select className={styles.select} value={schemaURI} onChange={handleChange}>
         <option>https://rickandmortyapi.com/graphql</option>
         <option>https://spacex-production.up.railway.app/</option>
